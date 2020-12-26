c2b44faf91e32e968cf5bc9abcf0da51625ceb20
raj713335 <raj713335@gmail.com>
2020-12-26 21:21:17 +0530

    Docker

--- code/hello_project/settings.py
+++ code/hello_project/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+
+    #Local
+    'pages.apps.PagesConfig',
 ]
 
 MIDDLEWARE = [
