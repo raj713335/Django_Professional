4fb092fc5d7439081ce957816c3b46bb1ca003b1
raj713335 <raj713335@gmail.com>
2020-12-28 18:34:43 +0530

    Bookstore Project

--- ch3-books/config/settings.py
+++ ch3-books/config/settings.py
@@ -69,8 +69,8 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'db',
+        'PASSWORD': 'myPassword',
+        'HOST': '127.0.0.1',
         'PORT': 5432
     }
 }
