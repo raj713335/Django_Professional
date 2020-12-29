e8b988e19a34f6118e49553ba2de464826312409
raj713335 <raj713335@gmail.com>
2020-12-29 18:29:51 +0530

    Reviews App

--- books/books/tests.py
+++ books/books/tests.py
@@ -1,16 +1,30 @@
 # books/tests.py
+
+from django.contrib.auth import get_user_model
 from django.test import Client, TestCase
 from django.urls import reverse
 
-from .models import Book
+from .models import Book, Review
 
 
 class BookTests(TestCase):
     def setUp(self):
+        self.user = get_user_model().objects.create_user(  # new
+            username='reviewuser',
+            email='reviewuser@email.com',
+            password='testpass123'
+        )
+
         self.book = Book.objects.create(
             title='Harry Potter',
             author='JK Rowling',
             price='25.00',
+        )
+
+        self.review = Review.objects.create(  # new
+            book=self.book,
+            author=self.user,
+            review='An excellent review',
         )
 
     def test_book_listing(self):
--- books/config/settings.py
+++ books/config/settings.py
@@ -86,7 +86,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
+        'NAME': 'professional',
         'USER': 'postgres',
         'PASSWORD': 'myPassword',
         'HOST': '127.0.0.1',
