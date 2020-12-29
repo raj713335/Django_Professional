4093e050c9966605475447d1899deccfdee73bfd
raj713335 <raj713335@gmail.com>
2020-12-30 00:18:35 +0530

    Search

--- books/books/views.py
+++ books/books/views.py
@@ -21,3 +21,9 @@ class BookDetailView(LoginRequiredMixin, PermissionRequiredMixin, DetailView):
     template_name = 'books/book_detail.html'
     login_url = 'account_login'
     permission_required = 'books.special_status'
+
+
+class SearchResultsListView(ListView):
+    model = Book
+    context_object_name = 'book_list'
+    template_name = 'books/search_results.html'
