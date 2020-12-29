fe245c5d29f881dacc30340b622e679149f61eb6
raj713335 <raj713335@gmail.com>
2020-12-29 17:10:35 +0530

    Books App

--- books/books/models.py
+++ books/books/models.py
@@ -1,5 +1,6 @@
 # books/models.py
 
+import uuid
 from django.db import models
 from django.urls import reverse
 
@@ -8,6 +9,7 @@ from django.urls import reverse
 
 
 class Book(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=200)
     price = models.DecimalField(max_digits=6, decimal_places=2)
